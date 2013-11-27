4c74028fc577e48a9c2558741ebe618f09374963
Valessio Brito <contato@valessiobrito.com.br>
2013-11-27 14:08:55 -0200

    Reposicionando a busca

--- views/participatorio/page/elements/header.php
+++ views/participatorio/page/elements/header.php
@@ -2,6 +2,11 @@
 
 echo elgg_view('page/elements/header_logo', $vars);
 
+$search = elgg_view('search/search_box');
+if ($search) {
+    echo '<div id="header-search">' . $search . '</div>';
+}
+
 echo elgg_view('core/account/login_dropdown');
 
 echo elgg_view('page/elements/toolbar', $vars);
