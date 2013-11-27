3c0a7fc97a2b558c86600eb3726b952df89f783b
Valessio Brito <contato@valessiobrito.com.br>
2013-11-27 15:42:43 -0200

    Add footer-page template

---
+++ views/participatorio/page/elements/footer-page.php
@@ -0,0 +1,14 @@
+<?php
+
+$powered_url = elgg_get_site_url() . "_graphics/powered_by_elgg_badge_drk_bckgnd.gif";
+$c3sl_icon = $site_url . "mod/hypeThemeX/graphics/powered_by_c3sl.png";
+?>
+<div class="mts clearfix float-alt">
+	<a href="http://www.c3sl.ufpr.br" target="_blank">
+		<img src="<?php echo $c3sl_icon; ?>" alt="Powered by C3SL" title="Powered and Hosted by C3SL" />
+	</a>
+	<a href="http://elgg.org" target="_blank">
+		<img src="<?php echo $powered_url; ?>" alt="Powered by Elgg" title="Powered by Elgg" width="106" height="15" />
+	</a>
+</div>
+<script src="http://barra.brasil.gov.br/barra.js?cor=cinza" type="text/javascript"></script>
